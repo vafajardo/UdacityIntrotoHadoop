b80d18ee31bbc937520bf07fe9b06ec339a12560
vafajardo <andrei.fajardo@gmail.com>
2016-06-03 06:05:55 -0400

    Remove part of code which omits "

--- Project/mapperPostAnswerLength.py
+++ Project/mapperPostAnswerLength.py
@@ -14,11 +14,10 @@ import datetime
 reader = csv.reader(sys.stdin, delimiter = '\t')
 next(reader, None) # to read the header line
 for line in reader:
-    data = [re.sub('"','',el) for el in line]
-    id = data[0]
-    body = data[4]
-    node_type = data[5]
-    parent_id = data[6]
+    id = line[0]
+    body = line[4]
+    node_type = line[5]
+    parent_id = line[6]
     if node_type == "question":
         print '{0}\tQ\t{1}'.format(id,len(body))
     if node_type == "answer":
